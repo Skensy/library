47a7e07ba2fbdd73387c678a69509f2f15d2c2e5
Skensy <11029252+Skensy@users.noreply.github.com>
2024-10-16 16:39:30 +0200

    working on required class of inputs, added tailwind css to books

--- src/script.js
+++ src/script.js
@@ -13,19 +13,24 @@ btn.addEventListener("click", () => {
       pages: numberPages.value,
       read: readStatus.value
     }
+
     event.preventDefault();
 
     myLibrary.push(bookOne);
-    console.log(myLibrary)
 
     let divOne = document.createElement("div");
     let titleDivOne = document.createElement("h1");
     let authorDivOne = document.createElement("h2");
     let pagesDivOne = document.createElement("h3");
     let readDivOne = document.createElement("p");
-    divOne.classList.add("flex", "flex", "flex-col", "w-80", "h-56", "bg-slate-500");
+
+    divOne.classList.add("flex", "flex", "flex-col", "w-80", "h-56", "bg-slate-200", "p-4", "rounded");
+    
     titleDivOne.textContent = bookOne.title;
+    titleDivOne.classList.add("text-xl", "mb-2")
+    
     authorDivOne.textContent = bookOne.author;
+    
     pagesDivOne.textContent = bookOne.pages;
     readDivOne.textContent = bookOne.read;
     
@@ -34,7 +39,16 @@ btn.addEventListener("click", () => {
     divOne.appendChild(pagesDivOne);
     divOne.appendChild(readDivOne);
     booksList.appendChild(divOne);
+
+
+    bookName.value = "";
+    authorName.value = "";
+    numberPages.value = "";
+    readStatus.value = "";
+
+
     event.preventDefault();
+
 }
 )
     
