848da10d7e166d39f9ad7ab0a41da1c25a15526f
Skensy <11029252+Skensy@users.noreply.github.com>
2024-10-16 15:44:48 +0200

    initial settings about css and selector

--- src/script.js
+++ src/script.js
@@ -1,19 +1,12 @@
 const myLibrary = [];
 
-const input = document.querySelector("input");
-const unli = document.querySelector("ul");
-const btn = document.querySelector("button");
-const unList = document.querySelector("ul")
+const bookName = document.querySelector("#bookName");
+const authorName = document.querySelector("#authorName");
+const numberPages = document.querySelector("#numberPages");
+const readStatus = document.querySelector("#readStatus")
 
- /**
-  * btn.addEventListener("click", () => {
-    let value = input.value;
-    const listValue = document.createElement("li");
-    listValue.innerText = value;
-    unList.appendChild(listValue);
-    input.value = "";
-    input.focus();
-  * 
-  * */
-
+btn.addEventListener("click", () => {
+    let book
+}
+)
     
